4cc9fbdb1390f055206b71c585070b4d82b73061
Maxwell Morgan <max@Maxwells-MacBook-Pro.local>
2024-09-26 17:08:20 -0400

    Scaffold web app files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Import Bootstrap styles
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import main SCSS file
+// import './styles/main.scss';
+
+// Survey component logic and behavior
+// Add your code here
