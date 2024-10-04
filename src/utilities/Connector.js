16f897543c7a08b3586591267c11d1177614398c
Max Morgan <maxwell.morgan@detroitmi.gov>
2024-10-04 16:04:03 -0400

    Implement API call

--- src/utilities/Connector.js
+++ src/utilities/Connector.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const API_URL = 'https://apis.detroitmi.gov/surveys/detroitmi_usability/';
+// const SURVEY_NAME = 'detroitmi_usability';
+const SURVEY_NAME = 'test_survey';
+const API_URL = `https://apis.detroitmi.gov/surveys/${SURVEY_NAME}/`;
+const TEST_RESPONSE = '{"survey_id": "123"}';
 
 export default class Connector {
   static formatData(surveyID, rawData) {
@@ -39,15 +42,18 @@ export default class Connector {
   static start(surveyID, rawData, credentials, success, fail){
     let request = Connector.buildRequest(surveyID, rawData, credentials);
     console.info('Sending request:', request);
-    request.text().then((bodyAsText) => {
-      console.info('Request body as text:', bodyAsText);
-    });
-    // fetch(request)
-    // .then((res) => {
-    //     success(res);
-    // })
-    // .catch((error) => {
-    //     fail(error);
-    // });
+    fetch(request)
+      .then(res => {
+        if (!res.ok) {
+            throw new Error(`Failed to post survey data. HTTP status ${res.status}. Message: ${res.statusText}`);
+        }
+        return res.json();
+      })
+      .then(data => {
+        success(data.survey_id);
+      })
+      .catch(error => {
+        fail(error);
+      });
   }
 }
